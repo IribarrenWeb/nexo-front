2472d0b6b7494a3d95dc062652936f535d278d66
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-07 00:59:26 +0100

    init user service

---
+++ src/services/user-service.jsx
@@ -0,0 +1,24 @@
+import { useFetch } from "../hooks/useFetch";
+
+const BASE_API = 'users/' 
+export const userService = () => {
+    const { execute } = useFetch();
+    
+    const store = async (formData) => {
+        try {
+            const { data, error } = await execute(`${BASE_API}`, 'POST', formData);
+
+            if (error) {
+                return toast.error(error.message);
+            }
+            
+            return data;
+        } catch (error) {
+            toast.error('Error desconocido')
+        }
+    }
+
+    return {
+        store,
+    }
+}
