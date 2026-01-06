755b1051c0f574541364d57dbbe7259d28e19e37
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-06 19:32:04 +0100

    change auth layout

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,12 +1,16 @@
-import { Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
     return (
-      <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
-        <div className="w-full max-w-md bg-white rounded-xl shadow-sm border border-gray-100 p-8">
-            <Outlet />
+        <div className="min-h-screen flex">
+            <div className='flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24 bg-[#111827] text-white'>
+                <div className="mx-auto w-full max-w-sm lg:w-96">
+                    <Outlet />
+                </div>
+            </div>
+            <div className="relative hidden w-0 flex-1 lg:block">
+            </div>
         </div>
-      </div>
     );
 }
 
