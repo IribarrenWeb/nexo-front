41f9349ee84cbb31ccd139d3afc0193d91bd5da7
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-06 19:34:32 +0100

    change pages and apply login form

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 const Profile = () => {
   return (
     <div className="text-center mt-20">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">Perfil de Usuario</h1>
+        <h1 className="text-4xl font-bold text-white mb-4">Perfil de Usuario</h1>
     </div>
   );
 };
