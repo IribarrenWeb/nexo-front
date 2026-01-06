6994fa5e7519c7398140f60a112d6b409b431d42
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-06 23:17:12 +0100

    fix login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import LoginForm from "../components/forms/LoginForm";
+import { authService } from "../services/auth-service";
 
 const Login = () => {
-  return (
-    <div className="flex items-center justify-center">
-      <div className="p-8 bg-white rounded-lg shadow-md w-96">
-        <h1 className="mb-4 text-2xl font-bold text-center text-blue-600">
-          Login
-        </h1>
-        <p className="text-gray-600">Bienvenido a clipper.</p>
-        <LoginForm />
-        <button className="w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600">
-          Entrar
-        </button>
+    const [data, setData] = useState({});
+
+    const { login } = authService();
+
+    const toLogin = () => {
+        login(data);
+    }
+
+    return (
+      <div className="flex items-center justify-center">
+        <div className="p-8 bg-white rounded-lg shadow-md w-96">
+          <h1 className="mb-4 text-2xl font-bold text-center text-indigo-500">
+            Login
+          </h1>
+
+          <LoginForm setData={setData} />
+
+          <button onClick={toLogin} className="mt-9 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+            Entrar
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
