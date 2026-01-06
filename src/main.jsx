76a1fc46c47fac136f94f25fef94e84a190506e2
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-06 23:15:54 +0100

    add toaster

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
+        <Toaster/>
         <App />
       </BrowserRouter>
     </AuthProvider>
