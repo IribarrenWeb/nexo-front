a1b62ac7341c59f57e7a6fb14142b2d112d91eae
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-08 00:45:36 +0100

    remove logs

--- src/components/guards/RoleGuard.jsx
+++ src/components/guards/RoleGuard.jsx
@@ -4,7 +4,6 @@ import { useAuth } from "../../context/AuthContext";
 export const RoleGuard = ({role}) => {
     const { user } = useAuth();
     
-    console.log(user.rol, role);
     if (user.rol !== role) {
         return <Navigate to="/" replace />
     }
