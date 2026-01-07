077d021752dda59dbdc1967de9cdc312bb9e5c6e
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-08 00:45:29 +0100

    fix guard imports

--- src/components/guards/RoleGuard.jsx
+++ src/components/guards/RoleGuard.jsx
@@ -1,12 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
-export const RoleGuard = (role) => {
+export const RoleGuard = ({role}) => {
     const { user } = useAuth();
     
+    console.log(user.rol, role);
     if (user.rol !== role) {
         return <Navigate to="/" replace />
     }
-    
+
     return <Outlet/>
 }
