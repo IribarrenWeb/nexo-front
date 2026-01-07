0a3c4e264d3d542df0179180e1cba9d58a5f6d42
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-07 23:53:21 +0100

    adapt for new rules and add reactive validation option

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,7 +1,7 @@
 import { useMemo, useState } from "react";
 import { useValidator, VALID_RULES } from "./useValidator";
 
-export const useForm = (schema, definitions = []) => {
+export const useForm = (schema, definitions = [], reactiveValidation = false) => {
     const [formValues, setFormValues] = useState(schema);
 
     const rules = useMemo(() => {
@@ -11,10 +11,13 @@ export const useForm = (schema, definitions = []) => {
             const rulesField = definitions.find(f => f?.name == key)?.rules ?? [];
             if (Array.isArray(rulesField) && rulesField?.length) {
                 rulesField.forEach(rule => {
+                    const parseRule = rule.split(':');
+                    const ruleParam = parseRule[1] ?? null;
+                    rule = parseRule[0];
                     if (VALID_RULES.includes(rule)) {
                         rules[key] = {
                             ...rules[key],
-                            [rule]: true,
+                            [rule]: ruleParam ?? true,
                         }
                     }
                 });
@@ -24,13 +27,14 @@ export const useForm = (schema, definitions = []) => {
         return rules;
     }, [definitions]);
     
-    const { errors, validateData, clearErrorKey, clearAllErrors } = useValidator(formValues, rules);
+    const { errors, validateData, clearErrorKey, clearAllErrors, validateField } = useValidator(formValues, rules);
 
     const handleChanges = (e) => {
         const { name, value } = e.target;
 
         setFormValues(pre => ({...pre, [name]: value}))
-        clearErrorKey(name);
+        if (reactiveValidation) validateField(name, value);
+        else clearErrorKey(name);
     }
 
     const resetForm = (e) => {
