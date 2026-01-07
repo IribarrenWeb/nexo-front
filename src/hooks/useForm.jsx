f9bfc88a448e3cd7fd193d4240e999055737a209
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-07 22:10:13 +0100

    integrate validator hook into form hook

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
+import useValidator from "./useValidator";
 
-export const useForm = (values) => {
+export const useForm = (values, rules = {}) => {
     const [formValues, setFormValues] = useState(values);
+    const { errors, validateData, clearErrorKey, clearAllErrors } = useValidator(formValues, rules);
 
     const handleChanges = (e) => {
         const { name, value } = e.target;
 
         setFormValues(pre => ({...pre, [name]: value}))
+        clearErrorKey(name);
     }
 
-    const resetForm = (e) => setFormValues(values)
+    const resetForm = (e) => {
+        setFormValues(values);
+        clearAllErrors();
+    }
 
     return {
         formValues,
+        errors,
         handleChanges,
         setFormValues,
-        resetForm
+        resetForm,
+        isValid: validateData,
     }
 }
