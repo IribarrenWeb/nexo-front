0195ab739d088283737a2c8002a2f52e5d61ce11
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-07 22:09:55 +0100

    implement ui input in register

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo } from "react";
 import { useForm } from "../../hooks/useForm";
 import { userModel } from "../../context/AuthContext";
+import Input from "../ui/Input";
 
 const formFields = [
     {
@@ -61,23 +62,16 @@ const RegisterForm = ({setData, errorFields, clearError}) => {
                             {label}
                         </label>
                         <div className="mt-2">
-                            <input
+                            <Input 
                                 name={name}
                                 type={type}
                                 required={required}
                                 value={formValues[name]}
-                                className={"block w-full rounded-md bg-gray-200 px-3 py-1.5 text-base text-grey-700 outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" + (errorFields.find(e => e.field === name) ? ' border-2 border-red-500' : '')}
                                 placeholder={label}
                                 onChange={handleChanges}
                                 onFocus={clearError.bind(this, name)}
+                                errorMessage={errorFields.find(e => e.field === name)?.message}
                             />
-                            {
-                                errorFields.find(e => e.field === name) ? (
-                                    <p className="mt-1 text-sm text-red-500">
-                                        {errorFields.find(e => e.field === name).message}
-                                    </p>
-                                ) : null
-                            }
                         </div>
                     </div>
                 ))
