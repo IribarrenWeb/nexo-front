d46670368861d9324fef8047dbb669c454b78938
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-08 00:04:01 +0100

    restructure form to implement new validator

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,74 +1,61 @@
-import { useEffect } from "react";
+import { useImperativeHandle } from "react";
 import { useForm } from "../../hooks/useForm";
+import Input from "../ui/Input";
 
 const userLogin = {
     username: '',
     password: ''
 }
 
-const LoginForm = ({setData, errorFields, clearError}) => {
-    const { formValues, handleChanges } = useForm({
-        ...userLogin
-    })
+const loginDefinitions = [
+    {
+        name: 'username',
+        type: 'text',
+        label: 'Username',
+        rules: ['required'],
+    },
+    {
+        name: 'password',
+        type: 'password',
+        label: 'Contraseña',
+        rules: ['required'],
+    }
+]
 
-    useEffect(() => {
-        setData({...formValues})
-    }, [formValues])
+const LoginForm = ({ ref }) => {
+    const { formValues, errors, handleChanges, isValid } = useForm({
+        ...userLogin
+    }, loginDefinitions)
+
+    useImperativeHandle(ref, () => ({
+        validateData: () => isValid(),
+        getFormValues: () => ({ ...formValues }),
+    }))
 
     return (
         <form action="#" method="POST" className="space-y-6">
-            <div>
-                <label
-                    className="block text-sm/6 font-medium text-gray-500"
-                >
-                    Username
-                </label>
-                <div className="mt-2">
-                    <input
-                        name="username"
-                        required
-                        value={formValues.username}
-                        className={ (errorFields.some(e=>e.field == 'username') ? "border-2 border-red-500" : '') + " block w-full rounded-md bg-gray-200 px-3 py-1.5 text-base text-gray-700 outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"}
-                        onChange={handleChanges}
-                        onFocus={clearError.bind(this, 'username')}
-                    />
-                    {
-                        errorFields.some(e => e.field === 'username') ? (
-                            <p className="mt-1 text-sm text-red-500">
-                                {errorFields.find(e => e.field === 'username').message}
-                            </p>
-                        ) : null
-                    }
-                </div>
-            </div>
+            {
+                loginDefinitions.map(({name, label, type}) => (
+                    <div>
+                        <label
+                            className="block text-sm/6 font-medium text-gray-500"
+                        >
+                            {label}
+                        </label>
+                        <div className="mt-2">
 
-            <div>
-                <div className="flex items-center justify-between">
-                    <label
-                        className="block text-sm/6 font-medium text-gray-500"
-                    >
-                        Contraseña
-                    </label>
-                </div>
-                <div className="mt-2">
-                    <input
-                        name="password"
-                        type="password"
-                        required
-                        value={formValues.password}
-                        onChange={handleChanges}
-                        onFocus={clearError.bind(this, 'password')}
-                        className={((errorFields.some(e=>e.field == 'password') ? 'border-2 border-red-500 ' : '')) + "block w-full rounded-md bg-gray-200 px-3 py-1.5 text-base text-gray-700 outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"}
-                    />
-                    {
-                        errorFields.some(e => e.field === 'password') ? (
-                            <p className="mt-1 text-sm text-red-500">
-                                {errorFields.find(e => e.field === 'password').message}
-                            </p>
-                        ) : null
-                    }
-                </div>
-            </div>
+                            <Input
+                                placeholder={label}
+                                name={name}
+                                type={type}
+                                value={formValues[name]}
+                                onChange={handleChanges}
+                                errorMessage={errors.find(e => e.field === name)?.message}
+                            />
+                        </div>
+                    </div>
+                ))
+            }
         </form>
     );
 }
