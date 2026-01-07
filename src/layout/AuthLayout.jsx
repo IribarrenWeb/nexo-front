5ae5f5ca2039fb039e472838922802ccb78b8520
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-07 01:02:00 +0100

    refactor auth layout

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,14 +1,41 @@
-import { Outlet } from 'react-router-dom'
+import { useMemo } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const AuthLayout = () => {
+    
+    const isRegister = useMemo(() => {
+        return window.location.pathname === '/register'
+    }, [window.location.pathname])
+
+    const navigate = useNavigate();
+
+    const redirectToLogin = () => {
+        navigate('/login')
+    }
+
     return (
         <div className="min-h-screen flex">
-            <div className='flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24 bg-[#111827] text-white'>
+            <div className={(isRegister ? 'hidden lg:flex' : '') + ' sm:px-6 lg:flex-none lg:px-20 xl:px-24 flex flex-1 flex-col justify-center px-4 py-12 bg-[#111827] text-white'}>
                 <div className="mx-auto w-full max-w-sm lg:w-96">
-                    <Outlet />
+                    {
+                        !isRegister ? <Outlet /> : <div>
+                            <h2 className="mt-6 text-3xl font-bold tracking-tight text-white">
+                                Crea tu cuenta
+                                </h2>
+                                <p className="mt-2 text-sm text-gray-300">
+                                    ¿Ya tienes una cuenta?{' '}
+                                    <a href="#" onClick={redirectToLogin} className="font-medium text-indigo-500 hover:text-indigo-400">
+                                        Inicia sesión
+                                    </a>
+                                </p>
+                        </div>
+                    }
                 </div>
             </div>
-            <div className="relative hidden w-0 flex-1 lg:block">
+            <div className={"relative  w-0 flex-1 " + (!isRegister ? "hidden lg:block" : "")}>
+                {
+                    isRegister ? <Outlet /> : ''
+                }
             </div>
         </div>
     );
