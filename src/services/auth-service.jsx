815ba71913fecd16297f563d631956428553a266
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-07 02:29:29 +0100

    refactor auth service

--- src/services/auth-service.jsx
+++ src/services/auth-service.jsx
@@ -5,15 +5,14 @@ import { useNavigate } from "react-router-dom";
 const BASE_API = 'auth/' 
 export const authService = () => {
     const { execute } = useFetch();
-    const navigate = useNavigate()
     
     const login = async (formData) => {
         try {
             const { data, error } = await execute(`${BASE_API}login`, 'POST', formData);
     
             if (data?.token) {
-                navigate('/')
-                return 
+                localStorage.setItem('access_token', data.token)
+                return data.user;
             }
 
             toast.error(error.message)
