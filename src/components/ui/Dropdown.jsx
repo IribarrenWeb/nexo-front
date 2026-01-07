01062be828cf3ef59ad1a6095cb515c87fa9e619
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-07 03:12:45 +0100

    add ui component

---
+++ src/components/ui/Dropdown.jsx
@@ -0,0 +1,42 @@
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
+
+export const Dropdown = ({children, actions}) => {
+    const [droped, setDroped] = useState(false);
+    
+    return (
+        <div className="relative inline-block text-left">
+            <div>
+                <button onClick={() => setDroped(true)} type="button" className="inline-flex w-full justify-center items-center rounded-md bg-opacity-10 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-20 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                    {children}
+                    {
+                        droped ? <ChevronDown className="-mr-1 ml-2 h-5 w-5 text-gray-400" aria-hidden="true" /> : <ChevronDown className="-mr-1 ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    }
+                </button>
+            </div>
+            
+            { droped ?
+                <>
+                    <div className="absolute z-50 right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="py-1">
+                            {actions.map((action, index) => (
+                                <a
+                                    key={index}
+                                    href={action.href || '#'}
+                                    onClick={action.onClick}
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                                >
+                                    {action.label}
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="backdrop bg-transparent z-40 fixed top-0 bottom-0 left-0 right-0" onClick={() => setDroped(false)}></div>
+                </> 
+                : null
+            }
+        </div>
+    )
+}
+
+export default Dropdown;
