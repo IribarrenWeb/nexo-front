30c7613672dbdb63f2fbf5056a63c863e1cdd1ef
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-08 22:52:10 +0100

    refactor Button component to support additional class names using cn helper

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,11 @@
-const Button = ({children, loading, onClick}) => {
+import { cn } from "../../utils/helpers"
+
+const Button = ({children, loading, onClick, className}) => {
     return (
         <button
             disabled={loading}
             onClick={onClick}
-            className="mt-9 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            className={cn('flex w-full justify-center rounded-md nx-accent-bg px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500', className)}
         >
             {loading ? 'Cargando...' : children}
         </button>
