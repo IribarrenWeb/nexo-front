c4241a51b6a4ab790e32a019aa49f480735d2e94
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-09 01:11:16 +0100

    add avatar field to RegisterForm with dynamic rendering based on form mode

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,76 +1,123 @@
-import { useEffect, useImperativeHandle } from "react";
+import { useImperativeHandle, useMemo } from "react";
 import { useForm } from "../../hooks/useForm";
 import { userModel } from "../../context/AuthContext";
 import Input from "../ui/Input";
+import Avatar from "../ui/Avatar";
 
+// definicion de los campos del formulario
+// showConditions: en que modos se muestra el campo
+// rules: reglas de validacion
 const formFields = [
+    {
+        name: 'avatar',
+        label: 'Avatar (URL)',
+        type: 'text',
+        rules: [],
+        showConditions: ['admin','edit']
+    },
     {
         name: 'username',
         label: 'Username',
         type: 'text',
         rules: ['required','minLength:4' ],
+        showConditions: ['register', 'admin']
     },
     {
         name: 'email',
         label: 'Email',
         type: 'email',
         rules: ['required','email'],
+        showConditions: ['register', 'admin']
     },
     {
         name: 'name',
         label: 'Nombre completo',
         type: 'text',
         rules: ['required','minLength:3'],
+        showConditions: ['register', 'admin']
     },
     {
         name: 'lastName',
         label: 'Apellido',
         type: 'text',
         rules: ['required'],
+        showConditions: ['register', 'admin']
     },
     {
         name: 'password',
         label: 'Contraseña',
         type: 'password',
         rules: ['required','password'],
+        showConditions: ['register', 'admin']
     },
     {
         name: 'rePassword',
         label: 'Repetir Contraseña',
         type: 'password',
         rules: ['required','match:password'],
+        showConditions: ['register']
     },
 ]
 
-const RegisterForm = ({ref}) => {
+// por defecto el modo es 'register', puede ser 'admin' o 'edit'
+const RegisterForm = ({ref, createMode = 'register'}) => {
+
+    // extraemos los campos validos segun el modo del formulario
+    const validFields = useMemo(() => {
+        return formFields.filter(field => field.showConditions.includes(createMode))
+    }, [createMode]);
+
     const { formValues, errors, handleChanges, isValid } = useForm({
         ...userModel
-    }, formFields, true);
+    }, validFields, true);
 
     useImperativeHandle(ref, () => ({
         validateForm: () => isValid(),
         getFormValues: () => ({...formValues}),
     }))
     
+    // funcion para renderizar el elemento segun su tipo
+    const renderElement = ({name, label, type}) => {
+        switch(name) {
+            case 'avatar':
+                return (
+                    <>
+                        <Avatar
+                            src={formValues['avatar']}
+                            size="lg"
+                            alt={`${formValues['name']} ${formValues['lastName']}`}
+                            className="mb-4 hover:bg-blue-900 transition-all duration-200 cursor-pointer"
+                        />
+                    </>
+                );
+            default:
+                return (
+                    <Input
+                        name={name}
+                        type={type}
+                        value={formValues[name]}
+                        placeholder={label}
+                        onChange={handleChanges}
+                        errorMessage={errors.find((e) => e.field === name)?.message}
+                    />
+                );
+        }
+
+    }
+    
     return (
         <form action="#" method="POST" className="space-y-6">
             {
-                formFields.map(({name, label, type}) => (
-                    <div key={name}>
+                validFields.map((data) => (
+                    <div key={data.name}>
                         <label
                             className="block text-sm/6 font-medium text-gray-500"
                         >
-                            {label}
+                            {data.label}
                         </label>
                         <div className="mt-2">
-                            <Input 
-                                name={name}
-                                type={type}
-                                value={formValues[name]}
-                                placeholder={label}
-                                onChange={handleChanges}
-                                errorMessage={errors.find(e => e.field === name)?.message}
-                            />
+                            {/* renderizamos dinamicamente el elemento */}
+                            {renderElement(data)}
                         </div>
                     </div>
                 ))
