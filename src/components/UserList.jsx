2fb721b6f41ad7c0689fb61bd1cc4d65cef0381d
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-09 01:12:45 +0100

    add UserList and modal for user management with dynamic user creation

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,7 +1,49 @@
+// definimos las columnas de la tabla
+// con name el nombre a mostrar y con accessor la propiedad del objeto
+const COLUMNS = [
+    {
+        name: 'ID',
+        accessor: 'id',
+    },
+    {
+        name: 'Nombre',
+        accessor: 'name',
+    },
+    {
+        name: 'Usuario',
+        accessor: 'username',
+    },
+    {
+        name: 'Email',
+        accessor: 'email',
+    },
+    {
+        name: 'Rol',
+        accessor: 'rol',
+    },
+    {
+        name: 'Estado',
+        accessor: 'deactivated',
+    },
+    {
+        name: 'Acciones',
+    }
+]
+
 const UserList = () => {
     return (
         <>
-            Listado de usuarios
+            <table className="min-w-full divide-y divide-gray-700">
+                <thead>
+                    <tr>
+                        {COLUMNS.map((column) => (
+                            <th key={column.name} className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                                {column.name}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+            </table>
         </>
     );
 }
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,10 +1,41 @@
+import { Plus } from "lucide-react";
+import Button from "../components/ui/Button";
 import UserList from "../components/UserList";
+import { useRef } from "react";
+import Modal from "../components/ui/Modal/Modal";
+import ModalTitle from "../components/ui/Modal/ModalTitle";
+import ModalContent from "../components/ui/Modal/ModalContent";
+import RegisterForm from "../components/forms/RegisterForm";
+import { useAuth } from "../context/AuthContext";
 
 const Users = () => {
+    const modalRef = useRef(null);
+    const { role } = useAuth();
+
     return (
-        <div className="text-center mt-20">
-            <UserList />
-        </div>
+        <>
+            <div className="flex items-center justify-between mb-8">
+                <h1 className="text-xl font-semibold text-white">Gestion de Usuarios</h1>
+                <div>
+                    <Button className="mt-0" onClick={() => modalRef.current.trigger()}>
+                        <div className="flex items-center">
+                            <Plus className="mr-2 h-4 w-4"/>
+                            Agregar usuario
+                        </div>
+                    </Button>
+                </div>
+            </div>
+            <div>
+                <UserList />
+            </div>
+
+            <Modal ref={modalRef}>
+                <ModalTitle>Crear nuevo usuario</ModalTitle>
+                <ModalContent>
+                    <RegisterForm createMode={role == 'admin' ? 'admin' : 'edit'} />
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
 
