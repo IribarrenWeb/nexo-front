7c79d73d1f0922f34befae9fc995cd6c30a2d348
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-10 20:36:39 +0100

    include in useForm hook input masking and async validation error handling

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -4,6 +4,7 @@ import { useValidator, VALID_RULES } from "./useValidator";
 export const useForm = (schema, definitions = [], reactiveValidation = false) => {
     const [formValues, setFormValues] = useState(schema);
 
+    // definicion de las reglas de validacion
     const rules = useMemo(() => {
         const rules = {};
         const keys = Object.keys(schema);
@@ -27,19 +28,40 @@ export const useForm = (schema, definitions = [], reactiveValidation = false) =>
         return rules;
     }, [definitions]);
     
-    const { errors, validateData, clearErrorKey, clearAllErrors, validateField } = useValidator(formValues, rules);
+    // hook validador
+    const { errors, validateData, clearErrorKey, clearAllErrors, validateField, setErrors } = useValidator(formValues, rules);
 
+    // maneja los cambios de los inputs del formulario
     const handleChanges = (e) => {
         const { name, value } = e.target;
-
-        setFormValues(pre => ({...pre, [name]: value}))
-        if (reactiveValidation) validateField(name, value);
+        const mask = definitions.find(def => def.name === name)?.mask;
+        
+        // si el campo tiene una funcion de mascara, la aplicamos al valor antes de setearlo
+        const maskedValue = mask && mask instanceof Function ? mask(value) : value;
+        
+        setFormValues(pre => ({...pre, [name]: maskedValue}))
+        if (reactiveValidation) validateField(name, maskedValue);
         else clearErrorKey(name);
     }
 
+    // resetea los valores del formulario
     const resetForm = (e) => {
         setFormValues(schema);
         clearAllErrors();
+    }
+
+    // funcion para setear errorres de validacion que vienen del servidor
+    const setAsyncValidations = (newErrors) => {
+        if (!Array.isArray(newErrors) || !newErrors.length) return; // si no es un array o esta vacio, no hacemos nada
+
+        // recorremos los errores y los añadimos al estado de errores
+        for (let index = 0; index < newErrors.length; index++) {
+            const { field, message } = newErrors[index];
+            setErrors(prev => {
+                const filtered = prev.filter(e => e.field !== field);
+                return [...filtered, {field, message}];
+            });
+        }
     }
 
     return {
@@ -49,5 +71,6 @@ export const useForm = (schema, definitions = [], reactiveValidation = false) =>
         setFormValues,
         resetForm,
         isValid: validateData,
+        setAsyncValidations
     }
 }
