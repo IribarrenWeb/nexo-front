1b6e80b969b6d794504f74c26c4c2c884b1652f3
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-11 14:18:47 +0100

    add new ui element textarea

---
+++ src/components/ui/Textarea.jsx
@@ -0,0 +1,26 @@
+import { cn } from "../../utils/helpers";
+
+const Textarea = ({ label, value, onChange, name, placeholder, className, errorMessage }) => {
+    const baseClasses = "w-full resize-none p-3 rounded-lg bg-transparent text-white border-1 border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500";
+    const errorClasses = errorMessage ? "border-red-500 focus:ring-red-500" : "";
+    const finalClasses = cn(baseClasses, errorClasses, className);
+
+    return (
+        <div className="mb-4">
+            {label && <label className="block text-white font-semibold mb-2">{label}</label>}
+            <textarea
+                className={finalClasses}
+                value={value}
+                name={name}
+                onChange={onChange}
+                placeholder={placeholder}
+                rows={4}
+            />
+            {
+                errorMessage && <p className="text-red-500 text-xs mt-1">{errorMessage}</p>
+            }
+        </div>
+    );
+}
+
+export default Textarea;
