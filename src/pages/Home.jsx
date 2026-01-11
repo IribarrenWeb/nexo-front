9c5478f81a8705a1f4b19c2e59e81c8dc9103fa3
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-11 16:00:56 +0100

    integrate posts components to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import PostCreator from "../components/PostCreator";
+import PostList from "../components/PostsList";
 
 const Home = () => {
   return (
-    <div className="bg-gray-950">
-      <PostCreator />
-    </div>
+    <>
+      <div className="bg-gray-950">
+        <PostCreator />
+      </div>
+
+      <div className="pt-7 border-t-2 border-gray-800 mt-10">
+        <PostList />
+      </div>
+    </>
   );
 };
 
