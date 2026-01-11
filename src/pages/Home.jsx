76c7418bd466d0d8459612aa002211852df7dd74
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-11 19:57:00 +0100

    add post selection functionality in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import { useRef } from "react";
 import PostCreator from "../components/PostCreator";
 import PostList from "../components/PostsList";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const listRef =  useRef(null);
+  const navigate = useNavigate();
 
   const pushNewPostToList = (newPost) => {
     listRef.current?.pushNewPost(newPost);
+  }
+
+  const onSelect = (post) => {
+    navigate(`/post/${post._id}`);
   }
 
   return (
@@ -16,7 +22,7 @@ const Home = () => {
       </div>
 
       <div className="pt-7 border-t-2 border-gray-800 mt-10">
-        <PostList ref={listRef} />
+        <PostList ref={listRef} onSelect={onSelect} />
       </div>
     </>
   );
