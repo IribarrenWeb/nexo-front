d85c9165a841591c2b6b250978294b3113c14a27
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-11 02:15:07 +0100

    change sidebar component for improved responsiveness and styling

--- src/components/general/Sidebar.jsx
+++ src/components/general/Sidebar.jsx
@@ -12,13 +12,13 @@ const menus = [
 const Sidebar = () => {
     const { user } = useAuth();
 
-    const linkBaseClass = "group flex gap-x-3 items-center text-gray-300 hover:text-white hover:bg-gray-800 transition rounded-md p-2 text-sm font-semibold leading-6";
+    const linkBaseClass = "group flex gap-x-3 items-center justify-center lg:justify-start text-gray-300 hover:text-white hover:bg-gray-800 transition rounded-md p-2 text-sm font-semibold leading-6";
     
     return (
-        <aside className="flex w-72 flex-col bg-blend-hard-light border-r border-gray-800 py-4 nx-aside">
+        <aside className="flex w-20 lg:w-72 transition-all flex-col bg-blend-hard-light border-r border-gray-800 py-4 nx-aside">
             <div className="flex h-16 items-center px-6 mb-5">
                 <img src="/images/nexo-logo.png" alt="nexo" className="h-8 w-auto mr-2" />
-                <h1 className="text-3xl font-bold text-white">Nexo.io</h1>
+                <h1 className="text-3xl font-bold text-white hidden lg:inline">Nexo.io</h1>
             </div>
             <nav className="flex-1 p-4 space-y-8 overflow-y-auto">
                 <ul className="space-y-1">
@@ -31,8 +31,8 @@ const Sidebar = () => {
                                         'bg-gray-800 text-white': window.location.pathname === menu.href
                                     })}
                                 >
-                                    <menu.icon className="h-5 w-5" />
-                                    {menu.name}
+                                    <menu.icon className="h-5 w-5 just" />
+                                    <span className="hidden lg:block">{menu.name}</span>
                                 </Link>
                             </li>
                         ))
