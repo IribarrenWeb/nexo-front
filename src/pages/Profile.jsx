fba224ca4103fb601e123d288c416b4c177ba877
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-11 22:50:50 +0100

    add follow/unfollow functionality and display user's posts in Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,18 +9,25 @@ import { toast } from "sonner";
 import { authService } from "../services/auth-service";
 import { userService } from "../services/user-service";
 import Loader from "../components/ui/Loader";
+import PostList from "../components/PostsList";
 
 const Profile = () => {
 	const { user, setUser } = useAuth();
 	
 	const { me } = authService();
-	const { showByUsername } = userService();
+	const { showByUsername, toFollow } = userService();
 
 	const { username } = useParams();
 	const [loading, setLoading] = useState(false);
+	const [loadingFollow, setLoadingFollow] = useState(false);
 	const [profileUser, setProfileUser] = useState(null);
 
-	const isAuthUser = !username || username === user.username;
+	const isAuthUser = useMemo(() => !username || username === user.username, [username, user]);
+	
+	const isAuthUserFollow = useMemo(() => {
+		if (isAuthUser) return false;
+		return profileUser?.followers?.includes(user._id);
+	}, [profileUser, user]);
 
 	const modalRef = useRef(null);
 	const fullName = useMemo(() => `${user.name} ${user.lastName}`, [user]);
@@ -45,11 +52,24 @@ const Profile = () => {
 		}
 	}
 
+	const triggerFollow = async () => {
+		try {
+			setLoadingFollow(true);
+			const updatedUser = await toFollow(profileUser._id);
+			setProfileUser(usr => ({...usr, ...updatedUser.model})); // actualizamos el usuario de perfil
+			setUser(usr => ({...usr, ...updatedUser.user})); // actualizamos el usuario autenticado
+			console.log(updatedUser, user, profileUser);
+		} catch (error) {
+			toast.error("Error al actualizar el seguimiento");
+		} finally {
+			setLoadingFollow(false);
+		}
+	}
+
 	useEffect(() => {
 		if (!user?._id && !username) return; // si no hay usuario autenticado y no hay username, no hacemos nada
-
 		loadUser(); // cargamos el usuario
-	}, [username, user]);
+	}, [username, user._id]);
 
 	// si no se encuentra el usuario
 	// mostramos un mensaje informativo
@@ -69,18 +89,18 @@ const Profile = () => {
 
 				{/* info de perfil */}
 				<div className="flex justify-center">
-					<div className="md:flex items-center justify-between min-w-5/6 lg:min-w-4/5">
-						<div className="flex justify-start min-w-1/2">
-							<div className="relative w-36 lg:w-40 xl:w-48 h-10 pt-4 pb-6 mr-1 lg:mr-2 xl:mr-4">
+					<div className="flex items-center justify-between min-w-5/6 lg:min-w-4/5">
+						<div className="lg:flex justify-start min-w-1/2">
+							<div className="relative w-30 lg:w-36 xl:w-40 h-10 pt-4 pb-6 mr-1 lg:mr-2 xl:mr-4">
 								<Avatar
 									size="xxxl"
 									src={profileUser.avatar}
 									alt={`${profileUser.name} ${profileUser.lastName}`}
-									className="mx-auto absolute -top-16 lg:-top-20 xl:-top-24 border-5 border-gray-950"
+									className="mx-auto absolute -top-10 lg:-top-16 xl:-top-20 border-5 border-gray-950"
 								/>
 							</div>
-							<div>
-								<div className="text-2xl lg:text-2xl xl:text-4xl font-bold capitalize mt-4">
+							<div className="mt-14 lg:mt-0">
+								<div className="text-2xl lg:text-3xl xl:text-4xl font-bold capitalize mt-4">
 									{fullName}
 								</div>
 								<div className="lg:text-1xl xl:text-2xl text-gray-400 font-light mt-2">
@@ -88,8 +108,16 @@ const Profile = () => {
 								</div>
 							</div>
 						</div>
-						<div className="flex items-center h-full justify-end lg:min-w-1/3">
-							<Button className="w-auto" onClick={() => modalRef.current.trigger()}>Editar perfil</Button>
+						<div className="flex items-start pt-3 lg:pt-0 lg:items-center h-full justify-end lg:min-w-1/6">
+							{
+								isAuthUser ? (
+									<Button className="w-auto" onClick={() => modalRef.current.trigger()}>Editar perfil</Button>
+								) : (
+									<Button loading={loadingFollow} outline={isAuthUserFollow} className="w-auto" onClick={triggerFollow}>
+										{isAuthUserFollow ? 'Dejar de seguir' : 'Seguir'}
+									</Button>
+								)
+							}
 						</div>
 					</div>
 				</div>
@@ -117,6 +145,9 @@ const Profile = () => {
 						<span className="font-bold mr-4">{profileUser.followers?.length}</span>
 						<span className="text-gray-400">Seguidores</span>
 					</div>
+				</div>
+				<div className="border-t-2 border-gray-800 mt-10 mb-0 pt-10">
+					<PostList userId={profileUser._id} />
 				</div>
 			</div>
 
