afbb67c80731eae32259cf1ff012b53818957834
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-11 14:18:01 +0100

    restructure app layout

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -18,26 +18,28 @@ const AppLayout = () => {
         });
     }, []);
 
-    const baseMainClasses = "relative border-2 border-gray-950 rounded-lg bg-gray-800 shadow-sm h-full";
+    const baseMainClasses = "relative border-2 border-gray-950 rounded-lg bg-gray-950 shadow-sm h-full";
     return (
         <div className="flex h-screen bg-gray-950 text-white overflow-hidden">
 
             <Sidebar />
 
-            <div className="flex-1 flex flex-col min-w-0">
+            <div className="flex-1 flex flex-col overflow-hidden">
                 <HeaderApp />
-                <main className="felx-1 overflow-y-auto p-8">
-                    <div className={cn(baseMainClasses)}>
-                        <Outlet />
-                    </div>
-                </main>
+                <div className="grid grid-cols-1 lg:grid-cols-4">
+                    <main className="felx-1 overflow-y-auto p-8 col-span-3 h-screen">
+                        <div className={cn(baseMainClasses)}>
+                            <Outlet />
+                        </div>
+                    </main>
+                    <aside className="hidden lg:block w-full sticky top-0 h-screen p-4">
+                        <div className="bg-gray-950 rounded-xl p-4 h-full">
+                            <h2 className="font-bold text-gray-200 mb-4">Nexos virales</h2>
+                        </div>
+                    </aside>
+                </div>
             </div>
 
-            {/* <aside className="hidden lg:block w-80 sticky top-0 h-screen p-4">
-                <div className="bg-gray-50 rounded-xl p-4 h-full">
-                    <h2 className="font-bold text-gray-500 mb-4">Clippers virales</h2>
-                </div>
-            </aside> */}
         </div>
     )
 }
