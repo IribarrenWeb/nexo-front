595dc00110880a7748dd7be3280f8c074c2d3990
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-12 01:15:00 +0100

    changes in Button component with navigation and disabled state handling

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,21 +1,31 @@
 import { Loader2 } from "lucide-react";
 import { cn } from "../../utils/helpers"
+import { useNavigate } from "react-router-dom";
 
-const Button = ({children, loading, onClick, className, size, outline}) => {
+const Button = ({children, loading, onClick, className, size, outline, to, disabled}) => {
     const sizes = {
         sm: 'nx-btn-sm',
         md: 'nx-btn-md',
         lg: 'nx-btn-lg',
     };
+    const navigate = useNavigate();
 
     let baseClasses = "flex w-full justify-center nx-btn";
     
     if (outline) baseClasses += " nx-btn-outline";
 
+    if (disabled) baseClasses += " nx-btn-disabled";
+    
+    const handleClick = (e) => {
+        if (loading || disabled) return;
+        if (to) navigate(to);
+        if (onClick) onClick(e);
+    }
+
     return (
         <button
-            disabled={loading}
-            onClick={onClick}
+            disabled={loading || disabled}
+            onClick={handleClick}
             className={cn(baseClasses, sizes[size] ?? '', className)}
         >
             {loading ? (
