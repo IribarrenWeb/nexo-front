2a620906defa35dff49e5c04944ee5ec96ace8a5
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-17 19:19:29 +0100

    improve loading state for posts lists

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,10 +3,13 @@ import { toast } from "sonner";
 import PostElement from "./PostElement";
 import InfiniteScroll from "./ui/InfiniteScroll";
 import { postService } from "../services/post-service";
+import { cn } from "../utils/helpers";
+import { Loader2Icon } from "lucide-react";
 
 const PostList = ({ref, userId, onSelect}) => {
     const [posts, setPosts] = useState([]);
     const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(true);
     const infiniteRef = useRef();
     const { index } = postService();
 
@@ -20,6 +23,7 @@ const PostList = ({ref, userId, onSelect}) => {
     const loadPosts = async () => {
         try {
             infiniteRef.current?.loading();
+            setLoading(true);
             const params = { page };
             if (userId) {
                 params.userId = userId;
@@ -32,8 +36,10 @@ const PostList = ({ref, userId, onSelect}) => {
             setPage(prevPage => prevPage + 1);
 
             if (posts.length === 0 || posts.length < 15) infiniteRef.current?.stop();
+            setLoading(false);
         } catch (error) {
             toast.error('Error al cargar los posts');
+            setLoading(false);
         } finally {
             infiniteRef.current?.loaded();
         }
@@ -49,17 +55,16 @@ const PostList = ({ref, userId, onSelect}) => {
         resetPosts();
     }, [userId]); // recargar los posts cuando cambie el userId
 
-    // mostrar mensaje si no hay posts
-    if (posts.length === 0 && !infiniteRef.current?.isLoading()) {
-        return (
-            <div className="p-5 text-center text-gray-500">
-                No hay posts para mostrar.
-            </div>
-        )
-    }
 
     return (
         <>
+            <div className={cn("p-5 text-center text-gray-500 hidden", {'block': !posts?.length && !loading})}>
+                No hay posts para mostrar.
+            </div>
+            <div className={cn("p-5 text-center text-gray-500 hidden", {'block': loading && !posts?.length})}>
+                <Loader2Icon className="animate-spin mx-auto mb-2" />
+                Cargando posts...
+            </div>
             <InfiniteScroll scrollTarget="#nx-app-main" offsetH={200} loadMore={loadPosts} ref={infiniteRef}>
                 {posts.map(post => (
                     <PostElement onClick={() => onSelect(post)} className="nx-posts" key={post._id} postData={post} />
